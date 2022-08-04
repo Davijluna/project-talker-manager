8b43b8440b09e94dfe8f5baf7e77b6004a462ffb
davi <davijluna@gmail.com>
2022-08-04 01:41:26 -0300

    refactor: adicionando + na linha 6

--- function/getTalkerId.js
+++ function/getTalkerId.js
@@ -3,7 +3,7 @@ const fs = require('fs/promises');
 async function getTalkerId(_request, response, next) {
     const TalkerId = JSON.parse(await fs.readFile('./talker.json', 'utf-8'));
     const { id } = _request.params;
-    const IdTalker = TalkerId.find((person) => person.id === Number(id));
+    const IdTalker = TalkerId.find((person) => person.id === +id);
     if (!IdTalker) { 
         return response.status(404).json({ message: 'Pessoa palestrante não encontrada' }); 
     }
