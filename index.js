f12feff848d4ad3ae43f1571528c9f0981d357d5
davi <davijluna@gmail.com>
2022-08-04 01:21:02 -0300

    feat:talker id

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { getAllTalkers } = require('./function/getAllTalkers');
+const { getTalkerId } = require('./function/getTalkerId');
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,6 +16,8 @@ app.get('/', (_request, response) => {
 });
 
 app.get('/talker', getAllTalkers);
+
+app.get('/talker/:id', getTalkerId);
 
 app.listen(PORT, () => {
   console.log('Online');
